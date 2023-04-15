45be3e589cd4a6fee2d6ed6fd39f6a09b65185b7
restlessronin <88921269+restlessronin@users.noreply.github.com>
2023-04-15 09:50:54 +0000

    remove superfluous

--- lib/openai_ex/chat_message.ex
+++ lib/openai_ex/chat_message.ex
@@ -3,14 +3,11 @@ defmodule OpenaiEx.ChatMessage do
   https://platform.openai.com/docs/guides/chat/introduction
   """
 
-  @api_fields [:role, :content]
-
   defp new(role, content) do
     %{
       role: role,
       content: content
     }
-    |> Map.take(@api_fields)
   end
 
   def system(content), do: new("system", content)
