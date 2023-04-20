009d1f68f28ff9d4eca346052b68bcbcc5eb03c7
restlessronin <88921269+restlessronin@users.noreply.github.com>
2023-04-20 15:43:14 +0000

    bump to 0.1.6

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule OpenaiEx.MixProject do
   use Mix.Project
 
-  @version "0.1.5"
+  @version "0.1.6"
   @description "Community maintained Elixir client for OpenAI API"
   @source_url "https://github.com/restlessronin/openai_ex"
 
