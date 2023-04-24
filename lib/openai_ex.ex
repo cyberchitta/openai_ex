08c91cefed0c3d4e7d67b46cfa8aa9619a33f0a8
restlessronin <88921269+restlessronin@users.noreply.github.com>
2023-04-24 08:48:57 +0000

    successful test run with finch

--- lib/openai_ex.ex
+++ lib/openai_ex.ex
@@ -10,6 +10,8 @@ defmodule OpenaiEx do
   @enforce_keys [:token]
   defstruct token: nil, organization: nil
 
+  @base_url "https://api.openai.com/v1"
+  @spec new(any, any) :: %OpenaiEx{organization: any, token: any}
   @doc """
   Creates a new OpenaiEx struct with the specified token and organization.
 
@@ -23,48 +25,66 @@ defmodule OpenaiEx do
   end
 
   @doc false
-  def middleware(openai = %OpenaiEx{}) do
-    mw = [{Tesla.Middleware.BaseUrl, "https://api.openai.com/v1"}]
-
+  def headers(openai = %OpenaiEx{}) do
     headers = [{"Authorization", "Bearer #{openai.token}"}]
 
     if is_nil(openai.organization) do
-      mw ++ [{Tesla.Middleware.Headers, headers}]
+      headers
     else
-      mw ++
-        [{Tesla.Middleware.Headers, headers ++ [{"OpenAI-Organization", openai.organization}]}]
+      headers ++ [{"OpenAI-Organization", openai.organization}]
     end
   end
 
   @doc false
-  def post(openai = %OpenaiEx{}, url, json: json) do
-    (middleware(openai) ++ [Tesla.Middleware.JSON])
-    |> Tesla.client()
-    |> Tesla.post!(url, json)
-    |> Map.get(:body)
-  end
-
   def post(openai = %OpenaiEx{}, url, multipart: multipart) do
-    (middleware(openai) ++ [Tesla.Middleware.DecodeJson])
-    |> Tesla.client()
-    |> Tesla.post!(url, multipart)
-    |> Map.get(:body)
+    body_stream = Multipart.body_stream(multipart)
+    content_length = Multipart.content_length(multipart)
+    content_type = Multipart.content_type(multipart, "multipart/form-data")
+
+    :post
+    |> Finch.build(
+      @base_url <> url,
+      headers(openai) ++
+        [{"Content-Type", content_type}, {"Content-Length", to_string(content_length)}],
+      {:stream, body_stream}
+    )
+    |> finch_run()
   end
 
   @doc false
-  def delete(openai = %OpenaiEx{}, url) do
-    (middleware(openai) ++ [Tesla.Middleware.DecodeJson])
-    |> Tesla.client()
-    |> Tesla.delete!(url)
-    |> Map.get(:body)
+  def post(openai = %OpenaiEx{}, url, json: json) do
+    :post
+    |> Finch.build(
+      @base_url <> url,
+      headers(openai) ++ [{"Content-Type", "application/json"}],
+      Jason.encode_to_iodata!(json)
+    )
+    |> finch_run()
   end
 
   @doc false
   def get(openai = %OpenaiEx{}, url) do
-    (middleware(openai) ++ [Tesla.Middleware.DecodeJson])
-    |> Tesla.client()
-    |> Tesla.get!(url)
-    |> Map.get(:body)
+    :get
+    |> Finch.build(@base_url <> url, headers(openai))
+    |> finch_run()
+  end
+
+  @doc false
+  def delete(openai = %OpenaiEx{}, url) do
+    :delete
+    |> Finch.build(@base_url <> url, headers(openai))
+    |> finch_run()
+  end
+
+  @doc false
+  def finch_run(finch_request) do
+    finch_request
+    |> Finch.request(OpenaiEx.Finch)
+    |> finch_result()
+  end
+
+  def finch_result({:ok, %{body: body}}) do
+    Jason.decode!(body)
   end
 
   @doc false
@@ -72,8 +92,8 @@ defmodule OpenaiEx do
     mp =
       req
       |> Map.drop(file_fields)
-      |> Enum.reduce(Tesla.Multipart.new(), fn {k, v}, acc ->
-        acc |> Tesla.Multipart.add_field(to_string(k), v)
+      |> Enum.reduce(Multipart.new(), fn {k, v}, acc ->
+        acc |> Multipart.add_part(Multipart.Part.text_field(v, k))
       end)
 
     req
@@ -85,7 +105,10 @@ defmodule OpenaiEx do
           c -> {"", c}
         end
 
-      acc |> Tesla.Multipart.add_file_content(content, filename, name: to_string(k))
+      acc
+      |> Multipart.add_part(
+        Multipart.Part.file_content_field(filename, content, k, filename: filename)
+      )
     end)
   end
 
