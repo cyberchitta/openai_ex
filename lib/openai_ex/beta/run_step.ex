38ab1bf7985a08f1df73c57912b7f49bf092b8ad
restlessronin <88921269+restlessronin@users.noreply.github.com>
2023-11-11 15:19:56 +0000

    first test of run step

---
+++ lib/openai_ex/beta/run_step.ex
@@ -0,0 +1,22 @@
+defmodule OpenaiEx.Beta.Runs.Step do
+  @moduledoc false
+  defp ep_url(thread_id, run_id, step_id \\ nil) do
+    "/threads/#{thread_id}/runs/#{run_id}/steps" <>
+      if is_nil(step_id), do: "", else: "/#{step_id}"
+  end
+
+  def retrieve(openai = %OpenaiEx{}, %{thread_id: thread_id, run_id: run_id, step_id: step_id}) do
+    openai
+    |> OpenaiEx.as_assistants_beta()
+    |> OpenaiEx.Http.get(ep_url(thread_id, run_id, step_id))
+  end
+
+  def list(openai = %OpenaiEx{}, params = %{thread_id: thread_id, run_id: run_id}) do
+    openai
+    |> OpenaiEx.as_assistants_beta()
+    |> OpenaiEx.Http.get(
+      ep_url(thread_id, run_id),
+      params |> Map.take(OpenaiEx.list_query_fields())
+    )
+  end
+end
