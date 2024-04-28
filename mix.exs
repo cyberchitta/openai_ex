496fb5c3854746015b8cf50545c015b0f724ca9a
restlessronin <88921269+restlessronin@users.noreply.github.com>
2024-04-28 08:04:47 +0000

    bump version

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule OpenaiEx.MixProject do
   use Mix.Project
 
-  @version "0.5.9"
+  @version "0.6.0"
   @description "Community maintained OpenAI API Elixir client for Livebook"
   @source_url "https://github.com/restlessronin/openai_ex"
 
