d2e610460f0b44335488143c5ff8ddd8587d6aaa
restlessronin <88921269+restlessronin@users.noreply.github.com>
2024-07-09 08:00:30 +0000

    add bang to thread runs

--- lib/openai_ex/beta/threads_runs.ex
+++ lib/openai_ex/beta/threads_runs.ex
@@ -71,18 +71,22 @@ defmodule OpenaiEx.Beta.Threads.Runs do
 
   See https://platform.openai.com/docs/api-reference/runs/createRun for more information.
   """
+  def create!(openai = %OpenaiEx{}, run = %{thread_id: _, assistant_id: _}, stream: true) do
+    openai |> create(run) |> Http.bang_it!()
+  end
+
   def create(openai = %OpenaiEx{}, run = %{thread_id: thread_id, assistant_id: _}, stream: true) do
-    openai
-    |> OpenaiEx.with_assistants_beta()
-    |> HttpSse.post(ep_url(thread_id),
-      json: run |> Map.take(@api_fields) |> Map.put(:stream, true)
-    )
+    json = run |> Map.take(@api_fields) |> Map.put(:stream, true)
+    openai |> OpenaiEx.with_assistants_beta() |> HttpSse.post(ep_url(thread_id), json: json)
+  end
+
+  def create!(openai = %OpenaiEx{}, run = %{thread_id: _, assistant_id: _}) do
+    openai |> create(run) |> Http.bang_it!()
   end
 
   def create(openai = %OpenaiEx{}, run = %{thread_id: thread_id, assistant_id: _}) do
-    openai
-    |> OpenaiEx.with_assistants_beta()
-    |> Http.post(ep_url(thread_id), json: run |> Map.take(@api_fields))
+    json = run |> Map.take(@api_fields)
+    openai |> OpenaiEx.with_assistants_beta() |> Http.post(ep_url(thread_id), json: json)
   end
 
   @doc """
@@ -99,6 +103,10 @@ defmodule OpenaiEx.Beta.Threads.Runs do
 
   https://platform.openai.com/docs/api-reference/runs/getRun
   """
+  def retrieve!(openai = %OpenaiEx{}, params = %{thread_id: _, run_id: _}) do
+    openai |> retrieve(params) |> Http.bang_it!()
+  end
+
   def retrieve(openai = %OpenaiEx{}, %{thread_id: thread_id, run_id: run_id}) do
     openai |> OpenaiEx.with_assistants_beta() |> Http.get(ep_url(thread_id, run_id))
   end
@@ -117,10 +125,13 @@ defmodule OpenaiEx.Beta.Threads.Runs do
 
   See https://platform.openai.com/docs/api-reference/assistants/modifyAssistant for more information.
   """
+  def update!(openai = %OpenaiEx{}, params = %{thread_id: _, run_id: _, metadata: _}) do
+    openai |> update(params) |> Http.bang_it!()
+  end
+
   def update(openai = %OpenaiEx{}, %{thread_id: thread_id, run_id: run_id, metadata: metadata}) do
-    openai
-    |> OpenaiEx.with_assistants_beta()
-    |> Http.post(ep_url(thread_id, run_id), json: %{metadata: metadata})
+    json = %{metadata: metadata}
+    openai |> OpenaiEx.with_assistants_beta() |> Http.post(ep_url(thread_id, run_id), json: json)
   end
 
   @doc """
@@ -149,10 +160,21 @@ defmodule OpenaiEx.Beta.Threads.Runs do
 
   https://platform.openai.com/docs/api-reference/runs/listRuns
   """
+  def list!(openai = %OpenaiEx{}, thread_id, params = %{} \\ %{}) do
+    openai |> list(thread_id, params) |> Http.bang_it!()
+  end
+
   def list(openai = %OpenaiEx{}, thread_id, params = %{} \\ %{}) do
-    openai
-    |> OpenaiEx.with_assistants_beta()
-    |> Http.get(ep_url(thread_id), params |> Map.take(OpenaiEx.list_query_fields()))
+    qry_params = params |> Map.take(OpenaiEx.list_query_fields())
+    openai |> OpenaiEx.with_assistants_beta() |> Http.get(ep_url(thread_id), qry_params)
+  end
+
+  def submit_tool_outputs!(
+        openai = %OpenaiEx{},
+        params = %{thread_id: _, run_id: _, tool_outputs: _},
+        stream: true
+      ) do
+    openai |> submit_tool_outputs(params, stream: true) |> Http.bang_it!()
   end
 
   def submit_tool_outputs(
@@ -168,6 +190,13 @@ defmodule OpenaiEx.Beta.Threads.Runs do
     )
   end
 
+  def submit_tool_outputs!(
+        openai = %OpenaiEx{},
+        params = %{thread_id: _, run_id: _, tool_outputs: _}
+      ) do
+    openai |> submit_tool_outputs(params) |> Http.bang_it!()
+  end
+
   def submit_tool_outputs(
         openai = %OpenaiEx{},
         %{thread_id: thread_id, run_id: run_id, tool_outputs: tool_outputs}
@@ -180,10 +209,12 @@ defmodule OpenaiEx.Beta.Threads.Runs do
     )
   end
 
+  def cancel!(openai = %OpenaiEx{}, params = %{thread_id: _, run_id: _}) do
+    openai |> cancel(params) |> Http.bang_it!()
+  end
+
   def cancel(openai = %OpenaiEx{}, %{thread_id: thread_id, run_id: run_id}) do
-    openai
-    |> OpenaiEx.with_assistants_beta()
-    |> Http.post(ep_url(thread_id, run_id, "cancel"))
+    openai |> OpenaiEx.with_assistants_beta() |> Http.post(ep_url(thread_id, run_id, "cancel"))
   end
 
   @car_fields [
@@ -203,12 +234,20 @@ defmodule OpenaiEx.Beta.Threads.Runs do
     :response_format
   ]
 
+  def create_and_run!(openai = %OpenaiEx{}, params = %{assistant_id: _}, stream: true) do
+    openai |> create_and_run(params, stream: true) |> Http.bang_it!()
+  end
+
   def create_and_run(openai = %OpenaiEx{}, params = %{assistant_id: _}, stream: true) do
     openai
     |> OpenaiEx.with_assistants_beta()
     |> HttpSse.post(@ep_url,
       json: params |> Map.take(@car_fields) |> Map.put(:stream, true)
     )
+  end
+
+  def create_and_run!(openai = %OpenaiEx{}, params = %{assistant_id: _}) do
+    openai |> create_and_run(params) |> Http.bang_it!()
   end
 
   def create_and_run(openai = %OpenaiEx{}, params = %{assistant_id: _}) do
